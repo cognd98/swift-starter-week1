631f65760fb7b618f51ba5e15aef6fcb6509e03f
cognd98 <139201159+cognd98@users.noreply.github.com>
2023-07-23 15:36:27 +0900

    first commit

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -10,38 +10,4 @@ import Foundation
 
 //step3
 
-func printBody(body: String, topping: String = "") {
-    print("\(topping)\(body)\(topping)")
-}
-
-func printLength(length: Int, body: String, topping: String) {
-    for _ in 0..<length {
-        printBody(body: body, topping: topping)
-    }
-}
-
-func printStick(stickLength: Int) {
-    for _ in 0..<stickLength {
-        print(" | | ")
-    }
-}
-
-func makePepero(lengthInfo: Int, bodyInfo: String, toppingInfo: String = "", stickInfo: Int) {
-    print("길이 : \(lengthInfo)")
-    print("몸통 : \(bodyInfo)")
-    print("토핑 : \(toppingInfo)")
-    print("막대길이 : \(stickInfo)")
-    print()
-    printLength(length: lengthInfo, body: bodyInfo, topping: toppingInfo)
-    printStick(stickLength: stickInfo)
-    print()
-}
-
-func printPepero() {
-    makePepero(lengthInfo: 10, bodyInfo: " *** ", stickInfo: 4)
-    makePepero(lengthInfo: 12, bodyInfo: "***" ,toppingInfo: "&", stickInfo: 4)
-    makePepero(lengthInfo: 12, bodyInfo: "***",toppingInfo: "#", stickInfo: 6)
-    makePepero(lengthInfo: 6, bodyInfo: " |0| ", stickInfo: 4)
-}
-
 printPepero()
---
+++ CodeStarterCamp_Week1/step3.swift
@@ -0,0 +1,44 @@
+//
+//  step3.swift
+//  CodeStarterCamp_Week1
+//
+//  Created by 이채훈 on 2023/07/19.
+//
+
+import Foundation
+
+
+func printBody(body: String, topping: String = "") {
+    print("\(topping)\(body)\(topping)")
+}
+
+func printLength(length: Int, body: String, topping: String) {
+    for _ in 0..<length {
+        printBody(body: body, topping: topping)
+    }
+}
+
+func printStick(stickLength: Int) {
+    for _ in 0..<stickLength {
+        print(" | | ")
+    }
+}
+
+func makePepero(lengthInfo: Int, bodyInfo: String, toppingInfo: String = "", stickInfo: Int) {
+    print("길이 : \(lengthInfo)")
+    print("몸통 : \(bodyInfo)")
+    print("토핑 : \(toppingInfo)")
+    print("막대길이 : \(stickInfo)")
+    print()
+    printLength(length: lengthInfo, body: bodyInfo, topping: toppingInfo)
+    printStick(stickLength: stickInfo)
+    print()
+}
+
+func printPepero() {
+    makePepero(lengthInfo: 10, bodyInfo: " *** ", stickInfo: 4)
+    makePepero(lengthInfo: 12, bodyInfo: "***" ,toppingInfo: "&", stickInfo: 4)
+    makePepero(lengthInfo: 12, bodyInfo: "***",toppingInfo: "#", stickInfo: 6)
+    makePepero(lengthInfo: 6, bodyInfo: " |0| ", stickInfo: 4)
+}
+
